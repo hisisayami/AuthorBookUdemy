361bb7714efff45383cd3ac69264fff852be27e8
Hisila Manandhar <hisilamanandhar@Hisilas-MacBook-Pro.local>
2019-08-27 23:30:18 -0400

    first commit

---
+++ src/main/java/com/spring5webapp/spring5webapp/model/Author.java
@@ -0,0 +1,91 @@
+package com.spring5webapp.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    //@Column ( name = "FirstName", nullable = false, length = 100)
+    private String firstName;
+
+    //@Column (name = "LastName", nullable = false, length = 100)
+    private String lastName;
+
+    @ManyToMany (mappedBy = "authorSet")
+    private Set<Book> bookSet = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, Set<Book> bookSet) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.bookSet = bookSet;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBookSet() {
+        return bookSet;
+    }
+
+    public void setBookSet(Set<Book> bookSet) {
+        this.bookSet = bookSet;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", bookSet=" + bookSet +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
